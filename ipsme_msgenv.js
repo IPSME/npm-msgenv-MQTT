4962f7fd7ea4ee49549e66bacac3d196e6015244
knev <knev@root-interface.se>
2022-09-02 22:14:21 +0200

    Use bind to this instead of global state.

--- ipsme_msgenv.js
+++ ipsme_msgenv.js
@@ -1,32 +1,37 @@
+
 const { systemPreferences } = require('electron')
 
 // https://www.electronjs.org/docs/latest/api/system-preferences#systempreferencessubscribenotificationevent-callback-macos
 // Under the hood this API subscribes to NSDistributedNotificationCenter
 // The object is the sender of the notification, and only supports NSString values for now.
 
-// we have to use the ID, rather than the handler itself to unsubsribe
-var global_subscription_ID= null;
-var global_subscription_handler= null;
-
 // https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
+
+
+//-------------------------------------------------------------------------------------------------
+// MsgEnv:
 
 // Normally you can specify an object to filter on when subscribing, but in electron that is missing
 // 
 exports.subscribe= function(handler) {
-    global_subscription_handler= handler;
-    global_subscription_ID= systemPreferences.subscribeNotification(null, (event, userInfo, object) => {
-        if (global_subscription_handler !== null)
-            global_subscription_handler(event);
-    });    
+	if (handler.subscription_ID !== undefined)
+		return;
+	console.log('MsgEnv: subscribe');
+    handler.subscription_ID= systemPreferences.subscribeNotification(null, function(event, userInfo, object) {
+        this(event);
+    }.bind(handler));    
 }
 
+// we have to use the ID, rather than the handler itself to unsubsribe
 exports.unsubscribe= function(handler) {
-    systemPreferences.unsubscribeNotification(global_subscription_ID);
-    global_subscription_handler= null;
+    systemPreferences.unsubscribeNotification(handler.subscription_ID);
+    delete handler.subscription_ID;
 }
 
 // Normally a {name, object, userInfo} tuple, but in electron it is apparently not possible
 // to specify the (sender) object explicitly; gets set automagically?!
-exports.publish= function(str_msg) {
-    systemPreferences.postNotification(str_msg, {}, true);
+exports.publish= function(msg) {
+    systemPreferences.postNotification(msg, {}, true);
 }
+
+//-------------------------------------------------------------------------------------------------
