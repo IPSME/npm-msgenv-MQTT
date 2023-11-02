b6d5ee3bb4f3346ff2b84ddab81e1f153e817655
knev <knev@users.noreply.github.com>
2023-11-02 09:48:48 +0100

    feat: ported code via ChatGPT.

--- src/ipsme_msgenv.cjs
+++ src/ipsme_msgenv.cjs
@@ -1,5 +1,6 @@
 
 const { systemPreferences } = require('electron');
+const mqtt = require('mqtt');
 const { BitLogr } = require ('@knev/bitlogr');
 
 // https://www.electronjs.org/docs/latest/api/system-preferences#systempreferencessubscribenotificationevent-callback-macos
@@ -53,22 +54,58 @@ var cfg_= (function() {
 //-------------------------------------------------------------------------------------------------
 // MsgEnv:
 
+let singleton;
+
+const kstr_TOPIC = 'IPSME';
+
+class Singleton {
+    constructor() {
+        this.client = mqtt.connect('mqtt://localhost:1883');
+
+        this.client.on('error', function (err) {
+            console.error('MQTT Connection Error:', err);
+        });
+
+        this.client.on('connect', function () {
+            // console.log('MQTT Connected');
+        });
+    }
+
+    static getInstance() {
+        if (!singleton) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
+
 // Normally you can specify an object to filter on when subscribing, but in electron that is missing
 // 
 function subscribe_(handler) {
 	if (handler.subscription_ID !== undefined)
 		return;
     LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: subscribe');
-    handler.subscription_ID= systemPreferences.subscribeNotification(cfg_.channel, function(event, userInfo, object) {
-        LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: onNotification: ', userInfo.msg);
-        this(userInfo.msg);
-    }.bind(handler));    
+    // handler.subscription_ID= systemPreferences.subscribeNotification(cfg_.channel, function(event, userInfo, object) {
+    //     LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: onNotification: ', userInfo.msg);
+    //     this(userInfo.msg);
+    // }.bind(handler));    
+
+    let instance = Singleton.getInstance();
+    handler.subscription_ID = function (topic, message) {
+        handler(message.toString());
+    };
+  
+    instance.client.on('message', handler.subscription_ID);
+    instance.client.subscribe(kstr_TOPIC);
 }
 
 // we have to use the ID, rather than the handler itself to unsubsribe
 function unsubscribe_(handler) {
     LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: unsubscribe');
-    systemPreferences.unsubscribeNotification(handler.subscription_ID);
+    // systemPreferences.unsubscribeNotification(handler.subscription_ID);
+    let instance = Singleton.getInstance();
+    instance.client.removeListener('message', handler.subscription_ID);
+    instance.client.unsubscribe(kstr_TOPIC);
     delete handler.subscription_ID;
 }
 
@@ -76,7 +113,9 @@ function unsubscribe_(handler) {
 // to specify the (sender) object explicitly; gets set automagically?!
 function publish_(msg) {
     LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: postNotification: ', msg);
-    systemPreferences.postNotification(cfg_.channel, { "msg" : msg }, true);
+    // systemPreferences.postNotification(cfg_.channel, { "msg" : msg }, true);
+    let instance = Singleton.getInstance();
+    instance.client.publish(kstr_TOPIC, msg);
 }
 
 //-------------------------------------------------------------------------------------------------
