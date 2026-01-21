faee2371412eecb0132612349e2b2cd177b5de2e
knev <knev@users.noreply.github.com>
2026-01-21 11:41:45 +0100

    feat: try building for cjs and es simutaneously again through ts.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,25 @@
-// import resolve from '@rollup/plugin-node-resolve';
-// import commonjs from '@rollup/plugin-commonjs';
-// import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
 	{
-		input: 'src/ipsme_msgenv.cjs',
-		output: {
-			name: "ipsme_msgenv",
-			file: 'dist/ipsme_msgenv.cjs.js',
-			format: 'cjs'
-		},
-		// plugins: [resolve()]
-	},
-	// {
-	// 	input: 'src/ipsme_msgenv.cjs',
-	// 	output: {
-	// 		name: "ipsme_msgenv",
-	// 		file: 'dist/ipsme_msgenv.es.mjs',
-	// 		format: 'es'
-	// 	},
-	// 	plugins: [resolve(), commonjs()]
-	// }
+		input: 'src/ipsme_msgenv.ts',
+		output: [
+			{ file: 'dist/ipsme_msgenv.cjs.js', format: 'cjs' },
+			{ file: 'dist/ipsme_msgenv.es.mjs', format: 'es' },
+		],
+		plugins: [
+			resolve({ preferBuiltins: true }),
+			commonjs(),
+			json(),
+			typescript({ tsconfig: './tsconfig.json' })
+		],
+		onwarn(warning, warn) {
+			if (warning.code === 'THIS_IS_UNDEFINED') return;
+			if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+			warn(warning);
+		}
+	}
 ];
--- src/ipsme_msgenv.ts
+++ src/ipsme_msgenv.ts
@@ -1,34 +1,32 @@
 
-const mqtt = require('mqtt');
-const { BitLogr } = require ('@knev/bitlogr');
+import mqtt, { MqttClient, IClientOptions } from 'mqtt';
+
+// const mqtt = require('mqtt');
+// const { BitLogr } = require ('@knev/bitlogr');
 
 // https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
 
 //-------------------------------------------------------------------------------------------------
 
-let LOGR_= new BitLogr();
+// let LOGR_= new BitLogr();
 
-const l_ = {
-	MsgEnv : 0b1 << 0,	// MsgEnv
-	CXNS : 0b1 << 1,	// connections
-	REFL : 0b1 << 2,	// reflection
-}
-LOGR_.labels= l_;
-
-// https://stackoverflow.com/questions/4602141/variable-name-as-a-string-in-javascript
-const __name = obj => Object.keys(obj)[0];
-// console.log('OUT', __name({variableName}) );
+// const l_ = {
+// 	MsgEnv : 0b1 << 0,	// MsgEnv
+// 	CXNS : 0b1 << 1,	// connections
+// 	REFL : 0b1 << 2,	// reflection
+// }
+// LOGR_.labels= l_;
 
 //-------------------------------------------------------------------------------------------------
 
-var cfg_= (function() {
-    let _options= {};
+interface MsgEnvOptions {
+  channel?: string;
+  prefix?: string;
+  // logr?: any;           // uncomment + type if you bring BitLogr back
+}
 
-    // options= {
-    // channel : 'IPSME',
-    //     prefix : '',
-    //     logr : ...
-    // }
+var cfg_= (function() {
+    let _options: MsgEnvOptions = {};
 
     return {
         get channel() {
@@ -40,8 +38,8 @@ var cfg_= (function() {
 		get options() { return _options; },
 		set options(obj) {
 			_options= obj;
-            if (_options.logr && _options.logr[ __name(l_) ] )
-			    LOGR_.toggled= _options.logr
+            // if (_options.logr && _options.logr[ __name(l_) ] )
+			//     // LOGR_.toggled= _options.logr
 		}
     }
 })();
@@ -49,11 +47,13 @@ var cfg_= (function() {
 //-------------------------------------------------------------------------------------------------
 // MsgEnv:
 
-let singleton;
-
 const kstr_TOPIC = 'IPSME';
 
-class Singleton {
+class MsgEnvSingleton {
+    private static _instance: MsgEnvSingleton | null = null;
+
+    public readonly client: MqttClient;
+
     constructor() {
         this.client = mqtt.connect('mqtt://localhost:1883');
 
@@ -66,11 +66,11 @@ class Singleton {
         });
     }
 
-    static getInstance() {
-        if (!singleton) {
-            singleton = new Singleton();
+    public static getInstance() : MsgEnvSingleton {
+        if (! MsgEnvSingleton._instance) {
+            MsgEnvSingleton._instance = new MsgEnvSingleton();
         }
-        return singleton;
+        return MsgEnvSingleton._instance;
     }
 }
 
@@ -79,13 +79,13 @@ class Singleton {
 function subscribe_(handler) {
 	if (handler.subscription_ID !== undefined)
 		return;
-    LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: subscribe');
+    // LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: subscribe');
     // handler.subscription_ID= systemPreferences.subscribeNotification(cfg_.channel, function(event, userInfo, object) {
     //     LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: onNotification: ', userInfo.msg);
     //     this(userInfo.msg);
     // }.bind(handler));    
 
-    let instance = Singleton.getInstance();
+    let instance = MsgEnvSingleton.getInstance();
     handler.subscription_ID = function (topic, message) {
         handler(message.toString());
     };
@@ -96,9 +96,9 @@ function subscribe_(handler) {
 
 // we have to use the ID, rather than the handler itself to unsubsribe
 function unsubscribe_(handler) {
-    LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: unsubscribe');
+    // LOGR_.log(l_.CXNS, cfg_.prefix +'MsgEnv: unsubscribe');
     // systemPreferences.unsubscribeNotification(handler.subscription_ID);
-    let instance = Singleton.getInstance();
+    let instance = MsgEnvSingleton.getInstance();
     instance.client.removeListener('message', handler.subscription_ID);
     instance.client.unsubscribe(kstr_TOPIC);
     delete handler.subscription_ID;
@@ -107,18 +107,18 @@ function unsubscribe_(handler) {
 // Normally a {name, object, userInfo} tuple, but in electron it is apparently not possible
 // to specify the (sender) object explicitly; gets set automagically?!
 function publish_(msg) {
-    LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: postNotification: ', msg);
+    // LOGR_.log(l_.REFL, cfg_.prefix +'MsgEnv: postNotification: ', msg);
     // systemPreferences.postNotification(cfg_.channel, { "msg" : msg }, true);
-    let instance = Singleton.getInstance();
+    let instance = MsgEnvSingleton.getInstance();
     instance.client.publish(kstr_TOPIC, msg);
 }
 
 //-------------------------------------------------------------------------------------------------
 
-module.exports= {
+export default {
     config : cfg_,
     subscribe : subscribe_,
     unsubscribe : unsubscribe_,
-    publish : publish_,
-    l : l_,
+    publish : publish_
+    // l : l_,
 }
